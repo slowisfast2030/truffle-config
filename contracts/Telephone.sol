c4e39859efff7f3a83ace0baa1b8c508b66f7fea
slowisfast2030 <slowisfast2030@163.com>
2023-02-24 11:35:04 +0800

    all is well

--- contracts/Telephone.sol
+++ contracts/Telephone.sol
@@ -5,6 +5,8 @@ import "truffle/console.sol";
 
 contract Telephone {
     uint public secret;
+    event secretSet(uint _value);
+    event returnEvent(uint _value);
 
     constructor(uint _secret) public payable { 
         console.log("Telephone::constructor\n\tsecret: %o", _secret); 
@@ -16,5 +18,16 @@ contract Telephone {
         console.log("The passphrase is: %o", secret);
         console.log("Leak it a hundred times, plus one: %o", telephoneSecret);
         return telephoneSecret; 
+    }
+
+    function getSecret() public view returns (uint) {
+        return secret;
+    }
+
+    function setSecret(uint _value) public {
+        secret = _value;
+        emit secretSet(_value);
+        emit returnEvent(_value+1);
+
     }
 }
